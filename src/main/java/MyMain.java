a9e4b1fde188bad7da658b72e97ae20d088beb99
22sdhoat <22sdhoat@athenian.org>
2020-09-09 00:48:35 +0000

    added functions that manipulate strings (palindrome, abc, and the)

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,26 +4,55 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int A_B_C = 0;    
+        for(int i = 0; i < str.length(); i++) {    
+            if(str.charAt(i) == 'a') {    
+                A_B_C++;
+            }
+            else if(str.charAt(i) == 'b') {   
+                A_B_C++;     
+            }
+            else if(str.charAt(i) == 'c') {  
+                A_B_C++;    
+            }
+        }
+        return A_B_C;
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
+        String THE = "the";
+        if (str.toLowerCase().indexOf(THE.toLowerCase()) != -1 ) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        int i = 0, j = str.length() - 1; 
+        while (i < j) { 
+            // If there is a mismatch 
+            if (str.charAt(i) != str.charAt(j)) 
+                return false; 
+            i++; 
+            j--; 
+        } 
+        return true; 
     }
-    
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
+        System.out.print("Str: ");
+		String abc = scan.nextLine();
+        System.out.println(countABC(abc));
+        System.out.print("Str: ");
+        String the = scan.nextLine();
+        System.out.println(containsThe(the));
+        System.out.print("Str: ");
+        String opposite = scan.nextLine();
+        System.out.println(isPalindrome(opposite));
     }
 }
